f6cb0c7f8e416d6aebc741130d47607a242ba9ef
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-10-12 15:04:36 +0900

    [Gold V] Title: 토마토, Time: 704 ms, Memory: 217480 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,53 +1,54 @@
-import sys
+
 from collections import deque
+import sys
+input = sys.stdin.readline
 
+x, y, z = map(int, input().split())
+box = [[[] for _ in range(y)] for _ in range(z)]
 
-m,n,h = map(int,sys.stdin.readline().split())
+# ←, ↑, →, ↓, z+1, z-1
+nx = [-1, 0, 1, 0, 0, 0]
+ny = [0, 1, 0, -1, 0, 0]
+nz = [0, 0, 0, 0, -1, 1]
 
-nx = [1,-1,0,0,0,0]
-ny =[0,0,1,-1,0,0]
-nz = [0,0,0,0,1,-1]
+completed = deque()
 
-ary = [[[] for _ in range(n)] for _ in range(h)]
+for zdx in range(z):
+    for ydx in range(y):
+        tomatoes = list(map(int, input().split()))
+        if tomatoes.count(1):
+            for xdx in range(x):
+                if tomatoes[xdx] == 1:
+                    completed.append((zdx, ydx, xdx))
+        
+        box[zdx][ydx] = tomatoes
 
+time = 0
+while len(completed) > 0:
+    time += 1
 
-queue = deque()
-for i in range(h):
-    for j in range(n):
-        ary[i][j] = list(map(int,sys.stdin.readline().split()))
-        for k in range(m):
-            if ary[i][j][k] ==1:
-                queue.append([i,j,k])
-max_count = 0
-count = 0
-while (queue):
-    count+=1
+    completed_size = len(completed)
 
-    current_queue = len(queue)
-    for _ in range(current_queue):
+    for _ in range(completed_size):
+        cur_z, cur_y, cur_x = completed.popleft()
 
-        element = queue.popleft()
-        i,j,k = element
-        for index in range(6):
-            pos_x =nx[index]+i
-            pos_y = ny[index]+j
-            pos_z = nz[index] + k
-            if 0<=pos_x<h and 0<=pos_y<n and 0<=pos_z<m:
-                if ary[pos_x][pos_y][pos_z]==0:
-                    queue.append([pos_x,pos_y,pos_z])
-                    ary[pos_x][pos_y][pos_z] = 1
+        box[cur_z][cur_y][cur_x] = 1
 
+        for idx in range(6):
+            next_z = cur_z + nz[idx]
+            next_y = cur_y + ny[idx]
+            next_x = cur_x + nx[idx]
+            
+            if 0 <= next_x < x and 0 <= next_y < y and 0 <= next_z < z:
+                if box[next_z][next_y][next_x] == 0:
+                    completed.append((next_z, next_y, next_x))
+                    box[next_z][next_y][next_x] = 1
 
-
-
-for i in range(h):
-    if max_count==-1:
-        break
-    for j in range(n):
-        if max_count==-1:
-            break
-        for k in range(m):
-            if ary[i][j][k] ==0:
+for zdx in range(z):
+    for ydx in range(y):
+        for xdx in range(x):
+            if box[zdx][ydx][xdx] == 0:
                 print(-1)
                 sys.exit(0)
-print(count-1)
+
+print(time-1)
