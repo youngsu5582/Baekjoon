268e0f60da6288f22ee46fe0d5954ef29f4b65d9
희선이 <98307410+youngsu5582@users.noreply.github.com>
2024-11-01 16:25:01 +0900

    [level 2] Title: 2 x n 타일링, Time: 6.15 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,21 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
-        int [] ary = new int[n+2];
-        ary[0] = 1;
-        ary[1] = 1;
-        for(int i= 2; i<= n+1;i++){
-            int prev = ary[i-2];
-            int value = ary[i-1];
-            int sum = prev + value;
-            ary[i] = sum > 1000000007 ? sum % 1000000007 : sum;
-        }
-        return ary[n] % 1_000_000_007;
+        int answer = 0;
+        return answer;
     }
-    
 }
-
-// 가로 길이가 2이고 세로의 길이가 1인 직사각형모양의 타일
-// 가로의 길이 n은 60,000이하의 자연수 입니다
-//  1,000,000,007으로 나눈 나머지를 return해주세요.
