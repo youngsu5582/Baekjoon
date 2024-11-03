b2629b7f3303c1aefb66c5f90be5c4a5e9f144e4
희선이 <98307410+youngsu5582@users.noreply.github.com>
2024-11-03 14:54:54 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -0,0 +1,21 @@
+-- 코드를 입력하세요
+SELECT CAR_ID,
+CASE 
+WHEN HISTORY_CAR_ID IS NULL THEN "대여 가능"
+WHEN HISTORY_CAR_ID IS NOT NULL THEN "대여중"
+END AS 'AVAILABILITY'
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY c
+LEFT JOIN 
+(SELECT CAR_ID as HISTORY_CAR_ID
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY c
+WHERE c.START_DATE <= '2022-10-16' AND c.END_DATE >= '2022-10-16' ) as HISTORY
+ON c.CAR_ID = HISTORY.HISTORY_CAR_ID
+GROUP BY c.CAR_ID
+ORDER BY c.CAR_ID DESC;
+
+
+
+# CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블에서 
+#2022년 10월 16일에 대여 중인 자동차인 경우 '대여중' 이라고 표시하고, 
+#대여 중이지 않은 자동차인 경우 '대여 가능'을 표시하는 컬럼(컬럼명: AVAILABILITY)을 추가하여 
+#자동차 ID와 AVAILABILITY 리스트를 출력하는 SQL문을 작성해주세요. 이때 반납 날짜가 2022년 10월 16일인 경우에도 '대여중'으로 표시해주시고 결과는 자동차 ID를 기준으로 내림차순 정렬해주세요.
