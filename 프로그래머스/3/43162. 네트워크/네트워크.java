0e6cc069f5101cf761ff42074ce3082c062fa58a
희선이 <98307410+youngsu5582@users.noreply.github.com>
2025-01-04 11:16:19 +0900

    [level 3] Title: 네트워크, Time: 3.37 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,25 +1,37 @@
+import java.util.*;
+
 class Solution {
-    static int len;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        len = n;
-        boolean[] visited = new boolean[n];
-        for(int i=0;i<len;i++){
+        boolean[] visited = new boolean[computers.length];
+        for(int i=0;i<computers.length;i++){
             if(visited[i]==false){
                 answer++;
-                visited[i]=true;
-                dfs(i,visited,computers);       
+                bfs(computers,i,visited);
             }
         }
-        
         return answer;
     }
+    public void dfs(int[][] computers,int index,boolean[] visited){
+        for(int j=0;j<computers.length;j++){
+            if(computers[index][j] == 1 && visited[j]==false){
+                visited[j]=true;
+                dfs(computers,j,visited);
+            }
+        }
+        return;
+    }
     
-    public void dfs(int index,boolean[] visited,int[][] computers){
-        for(int i=0;i<len;i++){
-            if(computers[index][i] == 1 && visited[i]==false){
-                visited[i]=true;
-                dfs(i,visited,computers);
+    public void bfs(int[][] computers,int index,boolean[] visited){
+        Deque<Integer> dq = new ArrayDeque<>();
+        dq.add(index);
+        while(!dq.isEmpty()){
+            int next = dq.poll();
+            visited[next]=true;
+            for(int i=0;i<computers.length;i++){
+                if(computers[next][i] == 1 &&visited[i]==false){
+                    dq.add(i);
+                }               
             }
         }
     }
