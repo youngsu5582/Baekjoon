b043c7c00331907d173f7c425ef9e5719acad34e
희선이 <98307410+youngsu5582@users.noreply.github.com>
2025-01-05 00:02:55 +0900

    [level 3] Title: 단속카메라, Time: 8.29 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -3,52 +3,17 @@ import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
         
-        var pq = init(routes);
-        int answer = 1;
-        Pair pair = pq.poll();
-        int right = pair.y;
+        Arrays.sort(routes,(arr1,arr2)->Integer.compare(arr1[1],arr2[1]));
+        int right = Integer.MIN_VALUE;
+        int answer = 0;
         
-        while(!pq.isEmpty()){
-            pair = pq.poll();
-            if(pair.x>right){
-                right = pair.y;
-                answer++;
-            }else{
-                right = Math.min(right,pair.y);
+        for(int i=0;i<routes.length;i++){
+            if(routes[i][0]>right){
+                ++answer;
+                right = routes[i][1];
             }
         }
         
         return answer;
     }
-    
-    private PriorityQueue<Pair> init(int[][] routes){
-        PriorityQueue<Pair> pq = new PriorityQueue<>((p1,p2)->Integer.compare(p1.x,p2.x));
-        
-        for(int[] route: routes){
-            pq.add(new Pair(route[0],route[1]));
-        }
-        return pq;
-    }
 }
-class Pair{
-    public int x;
-    public int y;
-    
-    public Pair(int x,int y){
-        this.x = x;
-        this.y = y;
-    }
-    
-    @Override
-    public String toString(){
-        return x+"\t"+y;
-    }
-    
-}
-
-// 차량의 대수는 1대 이상 10,000대 이하입니다.
-// routes에는 차량의 이동 경로가 포함되어 있으며 routes[i][0]에는 i번째 차량이 고속도로에 진입한 지점, routes[i][1]에는 i번째 차량이 고속도로에서 나간 지점이 적혀 있습니다.
-// 차량의 진입/진출 지점에 카메라가 설치되어 있어도 카메라를 만난것으로 간주합니다.
-// 차량의 진입 지점, 진출 지점은 -30,000 이상 30,000 이하입니다.
-
-// -20 ~ -15 , -18 ~ -13 , -14 ~ -5 , -5 ~ -3
