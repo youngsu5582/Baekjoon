43cb637c182bf9ea982b91012130408a68474280
희선이 <98307410+youngsu5582@users.noreply.github.com>
2025-01-15 11:20:52 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 132 ms, Memory: 15140 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -13,22 +13,85 @@ public class Main {
     // 2초, 512MB
 
     // 있는 위치 : N, 동생 위치 : K
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(final String[] args) throws IOException {
+
+        final StringTokenizer st = new StringTokenizer(br.readLine());
         final int n = Integer.parseInt(st.nextToken());
         final int k = Integer.parseInt(st.nextToken());
 
         final int[] ary = new int[Math.max(n, k) * 2 + 1];
-        Arrays.fill(ary, Integer.MAX_VALUE);
-        Deque<Pair> dq = new ArrayDeque<>();
+        Arrays.fill(ary, Integer.MAX_VALUE - 1);
+
+        dp(n, k, ary);
+//        bfs(n, k, ary);
+    }
+
+    // 5 -> 18
+    // 5 10 20 .. 19 .. 18
+    private static void dp(final int n, final int k, final int[] ary) throws IOException {
+        // 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+        // 0 1 2 3 4 0   1  2  3  4  5  6  7  8  9 0
+
+        // n /2 or n-1 or n + 1
+        // n * 2 를 자신 값으로 초기화
+        // n-1, n+1 도 자신 +1 값으로 초기화
+
+        int number = n;
+        for (int i = 0; i <= number; i++) {
+            ary[i] = number - i;
+        }
+        while (number < k && number != 0) {
+            final int length = Math.min(number * 2, ary.length);
+            for (int i = number; i < length; i++) {
+                ary[i] = i - number;
+            }
+            number *= 2;
+        }
+
+        if (n >= k) {
+            System.out.println(ary[k]);
+            return;
+        }
+
+        for (int i = 0; i <= k; i++) {
+            ary[i] = min(findHalf(n, ary), ary[i], find(i - 1, ary), find(i + 1, ary));
+            ary[i * 2] = ary[i];
+            ary[i + 1] = Math.min(find(i, ary), ary[i + 1]);
+
+        }
+        System.out.println(ary[k]);
+    }
+
+    private static int findHalf(int n, int[] ary) {
+        if (n % 2 != 0 || n / 2 == 0) {
+            return Integer.MAX_VALUE;
+        }
+        return ary[n / 2];
+    }
+
+    private static int find(int n, int[] ary) {
+        if (n < 0 || n >= ary.length) {
+            return Integer.MAX_VALUE;
+        }
+        return ary[n] + 1;
+    }
+
+    private static int min(int n1, int n2, int n3, int n4) {
+        return Math.min(Math.min(n1, n2), Math.min(n3, n4));
+    }
+
+    private static void bfs(final int n, final int k, final int[] ary) throws IOException {
+        final Deque<Pair> dq = new ArrayDeque<>();
         dq.add(new Pair(n, 0));
 
         while (!dq.isEmpty()) {
-            Pair p = dq.poll();
+            final Pair p = dq.poll();
             if (p.x == k) {
-                System.out.println(p.count);
+                bw.write(p.count);
+                bw.flush();
                 break;
             }
             if (p.x < 0 || p.x >= ary.length) {
@@ -36,7 +99,7 @@ public class Main {
             }
 
             dq.add(new Pair(p.x * 2, p.count));
-            int element = ary[p.x];
+            final int element = ary[p.x];
             if (element > p.count) {
                 dq.add(new Pair(p.x - 1, p.count + 1));
                 dq.add(new Pair(p.x + 1, p.count + 1));
@@ -44,14 +107,13 @@ public class Main {
             }
         }
     }
-
 }
 
 class Pair {
     int x;
     int count;
 
-    public Pair(int x, int count) {
+    public Pair(final int x, final int count) {
         this.x = x;
         this.count = count;
     }
