4908519773dcfba22036862caaefb6f442802855
희선이 <98307410+youngsu5582@users.noreply.github.com>
2025-02-24 17:21:05 +0900

    [Silver I] Title: 싫은데요, Time: 384 ms, Memory: 66616 KB -BaekjoonHub

--- 백준/Silver/25916. 싫은데요/싫은데요.java
+++ 백준/Silver/25916. 싫은데요/싫은데요.java
@@ -3,34 +3,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+import static java.lang.Math.max;
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+
         int[] ary = new int[n];
-        st = new StringTokenizer(br.readLine(), " ");
-        int index = 0;
-        while (st.hasMoreTokens()) {
-            ary[index++] = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            ary[i] = Integer.parseInt(st.nextToken());
         }
 
-        int tail_index = 0;
-        index = 0;
-        int prev = 0;
+        int left = 0;
+        int right = 0;
         int sum = 0;
-        while (index < n) {
-            int next = ary[index];
-            if (prev + next <= m) {
-                prev += next;
-                index++;
-                sum = Math.max(prev, sum);
-                continue;
+        int answer = 0;
+
+        while (left < n) {
+            if (sum + ary[left] <= m) {
+                sum += ary[left++];
+                answer = max(answer, sum);
+            } else {
+                sum -= ary[right++];
             }
-            prev -= ary[tail_index++];
         }
-        System.out.println(sum);
+        System.out.println(answer);
     }
 }
